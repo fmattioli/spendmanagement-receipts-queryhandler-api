30942e5464198b0e70efe2db1c2bde5a911398da
ext.felipe.mattioli <ext.felipe.mattioli@farfetch.com>
2023-05-08 19:26:54 +0100

    Test

--- src/SpendManagement.ReadModel/API/Program.cs
+++ src/SpendManagement.ReadModel/API/Program.cs
@@ -5,7 +5,7 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to  the container.
+// Add services to the container.
 
 builder.Services
     .AddDependencyInjection()
