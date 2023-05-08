0c0007636a47aacfcd3cebcb3ff3b6f122b53e44
ext.felipe.mattioli <ext.felipe.mattioli@farfetch.com>
2023-05-08 19:14:53 +0100

    FEAT: Adding swagger documentation

--- src/SpendManagement.ReadModel/API/Program.cs
+++ src/SpendManagement.ReadModel/API/Program.cs
@@ -1,6 +1,8 @@
 using API.Extensions;
 using CrossCutting.Extensions.Logging;
 
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -11,7 +13,11 @@ builder.Services
     .AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "SpendManagement - ReadModel", Version = "v1" });
+    c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "Spents.API.xml"));
+});
 
 var app = builder.Build();
 
