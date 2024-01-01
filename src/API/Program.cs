2d93924f2b94785f74c0ad1683d6aa9d5790590c
Felipe Mattioli <felipe-mattioli98@hotmail.com>
2024-01-01 16:14:56 -0300

    FIX: Solving errors related to the .NET update version

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -17,6 +17,11 @@ builder.Configuration
 
 var applicationSettings = builder.Configuration.GetSection("Settings").Get<Settings>();
 
+builder.Logging
+    .ClearProviders()
+    .AddFilter("Microsoft", LogLevel.Warning)
+    .AddFilter("Microsoft", LogLevel.Critical);
+
 // Add services to the container.
 builder.Services
     .AddTracing(applicationSettings!.TracingSettings)
