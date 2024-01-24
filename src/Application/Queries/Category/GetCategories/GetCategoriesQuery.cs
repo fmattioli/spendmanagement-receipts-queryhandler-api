deb1ad34db2a6448b268665c44b7b54bdd4b20e4
Felipe Mattioli <felipe-mattioli98@hotmail.com>
2024-01-24 19:33:57 -0300

    Removing appsettings.Docker

--- src/Application/Queries/Category/GetCategories/GetCategoriesQuery.cs
+++
@@ -1,7 +0,0 @@
-﻿using MediatR;
-
-namespace Application.Queries.Category.GetCategories
-{
-    public record GetCategoriesQuery(GetCategoriesRequest GetReceiptsRequest) : IRequest<GetCategoriesResponse>;
-}
-
--- src/Domain/Entities/Category.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Domain.Entities
-{
-    public class Category
-    {
-        public Guid Id { get; set; }
-        public string Name { get; set; } = null!;
-        public DateTime CreatedDate { get; set; }
-    }
-}
